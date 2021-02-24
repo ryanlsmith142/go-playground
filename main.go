c97ba212989b0ee0c112361d73807d459c5b6b3a
Ryan Smith <ryan.l.smith142@gmail.com>
2021-02-23 21:36:46 -0600

    Adding some arrays

--- main.go
+++ main.go
@@ -11,11 +11,18 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("")
+	message, err := greetings.Hello("Ryan")
 
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	var array [2]string
+
+	array[0] = "Hello"
+	array[1] = "World"
+
+	fmt.Println(array[0], array[1])
 
 	fmt.Println(message)
 }
