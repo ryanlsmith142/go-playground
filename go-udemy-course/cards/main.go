a637d9c21eeb9c6287057571ec401ae128b29729
Ryan Smith <ryan.l.smith142@gmail.com>
2021-06-02 16:12:29 -0500

    Adds deck to string function

--- go-udemy-course/cards/main.go
+++ go-udemy-course/cards/main.go
@@ -3,14 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	// cards := newDeck()
-
-	// hand, remainingCards := deal(cards, 5)
-
-	// hand.print()
-	// remainingCards.print()
-
-	greeting := "Hi there!"
-	fmt.Println([]byte(greeting))
+	cards := newDeck()
+	fmt.Println(cards.toString())
 
 }
